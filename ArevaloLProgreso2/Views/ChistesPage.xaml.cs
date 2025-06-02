efeb68a98e468a8fb5d903878baced8e7a1c8268
Leninprog <lenin.arevalo@udla.edu.ec>
2025-06-02 07:27:21 -0500

    Creacion de las primeras views, pagina principal y pagina de chistes

---
+++ ArevaloLProgreso2/Views/ChistesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ArevaloLProgreso2.Views;
+
+public partial class ChistesPage : ContentPage
+{
+	public ChistesPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ArevaloLProgreso2/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ArevaloLProgreso2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAboutClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+
+        private async void OnChisteClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ChistesPage());
+        }
+    }
+
+}
+
